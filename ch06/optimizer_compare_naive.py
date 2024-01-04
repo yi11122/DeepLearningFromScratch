d9464a54e338b4b2380325ec98b591f9d0dee5a5
Xu ZeHui <78083186+yi11122@users.noreply.github.com>
2024-01-04 08:39:52 +0800

    first

--- ch06/optimizer_compare_naive.py
+++ ch06/optimizer_compare_naive.py
@@ -29,42 +29,80 @@ optimizers["Adam"] = Adam(lr=0.3)
 
 idx = 1
 
-for key in optimizers:
-    optimizer = optimizers[key]
-    x_history = []
-    y_history = []
-    params['x'], params['y'] = init_pos[0], init_pos[1]
-    
-    for i in range(30):
-        x_history.append(params['x'])
-        y_history.append(params['y'])
-        
-        grads['x'], grads['y'] = df(params['x'], params['y'])
-        optimizer.update(params, grads)
-    
+key = 'Adam'
 
-    x = np.arange(-10, 10, 0.01)
-    y = np.arange(-5, 5, 0.01)
-    
-    X, Y = np.meshgrid(x, y) 
-    Z = f(X, Y)
-    
-    # for simple contour line  
-    mask = Z > 7
-    Z[mask] = 0
-    
-    # plot 
-    plt.subplot(2, 2, idx)
-    idx += 1
-    plt.plot(x_history, y_history, 'o-', color="red")
-    plt.contour(X, Y, Z)
-    plt.ylim(-10, 10)
-    plt.xlim(-10, 10)
-    plt.plot(0, 0, '+')
-    #colorbar()
-    #spring()
-    plt.title(key)
-    plt.xlabel("x")
-    plt.ylabel("y")
+optimizer = optimizers[key]
+x_history = []
+y_history = []
+params['x'], params['y'] = init_pos[0], init_pos[1]
+
+for i in range(30):
+    x_history.append(params['x'])
+    y_history.append(params['y'])
+
+    grads['x'], grads['y'] = df(params['x'], params['y'])
+    optimizer.update(params, grads)
+
+x = np.arange(-10, 10, 0.01)
+y = np.arange(-5, 5, 0.01)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+
+# for simple contour line
+mask = Z > 7
+Z[mask] = 0
+
+# plot
+# plt.subplot(2, 2, idx)
+# idx += 1
+plt.plot(x_history, y_history, 'o-', color="red")
+plt.contour(X, Y, Z)
+plt.ylim(-10, 10)
+plt.xlim(-10, 10)
+plt.plot(0, 0, '+')
+# colorbar()
+# spring()
+plt.title(key)
+plt.xlabel("x")
+plt.ylabel("y")
+
+# for key in optimizers:
+#     optimizer = optimizers[key]
+#     x_history = []
+#     y_history = []
+#     params['x'], params['y'] = init_pos[0], init_pos[1]
+#
+#     for i in range(30):
+#         x_history.append(params['x'])
+#         y_history.append(params['y'])
+#
+#         grads['x'], grads['y'] = df(params['x'], params['y'])
+#         optimizer.update(params, grads)
+#
+#
+#     x = np.arange(-10, 10, 0.01)
+#     y = np.arange(-5, 5, 0.01)
+#
+#     X, Y = np.meshgrid(x, y)
+#     Z = f(X, Y)
+#
+#     # for simple contour line
+#     mask = Z > 7
+#     Z[mask] = 0
+#
+#     # plot
+#     plt.subplot(2, 2, idx)
+#     idx += 1
+#     plt.plot(x_history, y_history, 'o-', color="red")
+#     plt.contour(X, Y, Z)
+#     plt.ylim(-10, 10)
+#     plt.xlim(-10, 10)
+#     plt.plot(0, 0, '+')
+#     #colorbar()
+#     #spring()
+#     plt.title(key)
+#     plt.xlabel("x")
+#     plt.ylabel("y")
     
 plt.show()
